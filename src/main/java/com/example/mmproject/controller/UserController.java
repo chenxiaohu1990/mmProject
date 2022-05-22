aefef06228bd34d52741b538b15122dbfc6f6e6d
chenxiaohu <369327270@qq.com>
2022-05-22 20:54:21 +0800

    更换注解

--- src/main/java/com/example/mmproject/controller/UserController.java
+++ src/main/java/com/example/mmproject/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.mmproject.pojo.user.User;
 import com.example.mmproject.userservice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/getUserInfo")
+    @PostMapping("/getUserInfo")
     public ResultVO getUserInfo(User user){
         return userService.getUserInfo(user);
     }
